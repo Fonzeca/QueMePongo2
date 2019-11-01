1f08fd12589b5002b4d1fa44e4ae693d9ca4d314
Alexis <alexisfonzos_@hotmail.com>
2019-11-01 17:43:47 -0300

    Avanzando con api preferncias

---
+++ Back/src/main/java/quemepongo2/api/PreferenciasController.java
@@ -0,0 +1,10 @@
+package main.java.quemepongo2.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PreferenciasController {
+
+	
+	
+}
---
+++ Back/src/main/java/quemepongo2/manager/PreferenciaService.java
@@ -0,0 +1,17 @@
+package main.java.quemepongo2.manager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import main.java.quemepongo2.persistence.PreferenciaRepository;
+
+@Service
+public class PreferenciaService {
+	
+	@Autowired
+	PreferenciaRepository repo;
+	
+	
+	
+	
+}
