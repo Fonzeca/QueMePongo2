a3073943e33161603c571fbdcf428fa5a6f2a75b
Jere <Pozzee21@gmail.com>
2019-11-02 02:58:25 -0300

    Cambio de la llamada a la api a homefragment

--- Front/app/src/main/java/com/s21/quemepongo2front/MainActivity.java
+++ Front/app/src/main/java/com/s21/quemepongo2front/MainActivity.java
@@ -1,25 +1,14 @@
 package com.s21.quemepongo2front;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -32,8 +21,10 @@ import androidx.navigation.ui.NavigationUI;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    Button botonlogin, botonhome;
+    Button botonlogin, botonhome, botonobjetos;
     String temperatura, nombre, viento, humedad;
+    int visivilidad;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
 
-        //Inicializar el navigation `
+        //Inicializar el navigation_bar
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_objetos_personales,
                 R.id.nav_ubicaciones, R.id.nav_share, R.id.nav_nuevo_usuario)
@@ -63,66 +54,10 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navigationView, navController);
 
 ////TODO: configurar el viento en km/h esta en Metros por segundo
-        AsyncTask.execute(new Runnable() {
-            @Override
-
-            public void run() {
-                try {
-                    String urlS = getString(R.string.ipApi)+ getString(R.string.json_pronostico);
-                    JSONObject jsonObject= new JSONObject(readUrl(urlS));
-
-                    temperatura = jsonObject.get("temperatura").toString();
-                    nombre = jsonObject.get("ciudadNombre").toString();
-
-                    // viento= jsonObject.get("viento").toString();
-                    //humedad=jsonObject.get("humedad").toString();
-                    TextView temp_actual = findViewById(R.id.temperatura_actual);
-                    temp_actual.setText(temperatura+"℃");
-                    TextView ubicacion = findViewById(R.id.textViewUbicacion);
-                    ubicacion.setText("Ubicacion: "+nombre);
-                    // TextView viento =findViewById(R.id.textViento);
-                    // viento.setText("Viento: "+viento+" km/h");
-
-                    TextView textHumedad=findViewById(R.id.textHumedad);
-                    textHumedad.setText("Humedad: "+humedad+"%");
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });
-
 
     }
-
-
-    private static String readUrl(String urlString) throws Exception {
-        BufferedReader reader = null;
-        try {
-            URL url = new URL(urlString);
-            reader = new BufferedReader(new InputStreamReader(url.openStream()));
-            StringBuffer buffer = new StringBuffer();
-            int read;
-            char[] chars = new char[1024];
-            while ((read = reader.read(chars)) != -1)
-                buffer.append(chars, 0, read);
-
-            return buffer.toString();
-        } finally {
-            if (reader != null)
-                reader.close();
-        }
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
@@ -133,8 +68,6 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
-    int visivilidad;
-    Button botonobjetos;
 
     public void mostrarobjetos(View v){
 
@@ -145,4 +78,3 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(gologin);
         }
     }
-
--- Front/app/src/main/java/com/s21/quemepongo2front/ui/home/HomeFragment.java
+++ Front/app/src/main/java/com/s21/quemepongo2front/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.s21.quemepongo2front.ui.home;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,15 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.s21.quemepongo2front.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +27,25 @@ import androidx.lifecycle.ViewModelProviders;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    String temperatura, nombre, viento, humedad;
+
+    private static String readUrl(String urlString) throws Exception {
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(urlString);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuffer buffer = new StringBuffer();
+            int read;
+            char[] chars = new char[1024];
+            while ((read = reader.read(chars)) != -1)
+                buffer.append(chars, 0, read);
+
+            return buffer.toString();
+        } finally {
+            if (reader != null)
+                reader.close();
+        }
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +59,45 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+
+            public void run() {
+                try {
+                    String urlS = getString(R.string.ipApi)+ getString(R.string.json_pronostico);
+                    JSONObject jsonObject= new JSONObject(readUrl(urlS));
+
+                    temperatura = jsonObject.get("temperatura").toString();
+                    nombre = jsonObject.get("ciudadNombre").toString();
+
+                    // viento= jsonObject.get("viento").toString();
+                    //humedad=jsonObject.get("humedad").toString();
+                    TextView temp_actual = getView().findViewById(R.id.temperatura_actual);
+                    temp_actual.setText(temperatura+"℃");
+                    TextView ubicacion = getView().findViewById(R.id.textViewUbicacion);
+                    ubicacion.setText("Ubicacion: "+nombre);
+                    // TextView viento =findViewById(R.id.textViento);
+                    // viento.setText("Viento: "+viento+" km/h");
+
+                    TextView textHumedad=getView().findViewById(R.id.textHumedad);
+                    textHumedad.setText("Humedad: "+humedad+"%");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
         return root;
     }
 }
+
+
+
