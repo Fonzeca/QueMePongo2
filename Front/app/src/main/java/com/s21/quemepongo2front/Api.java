02df17d41b128e0b0b4b462e223f3284e6f180c7
Alexis <alexisfonzos_@hotmail.com>
2019-11-02 23:26:33 -0300

    Creando singleton de Retrofit

---
+++ Front/app/src/main/java/com/s21/quemepongo2front/Api.java
@@ -0,0 +1,24 @@
+package com.s21.quemepongo2front;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Api {
+
+    private static Retrofit retrofit = null;
+    private static String apiUrl = "http://181.31.108.164:5599/";
+//    private static String apiUrl = "http://192.167.0.7:5599/"; PC DIEGO
+
+    public static Retrofit getRetrofit(){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(apiUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+
+
+}
