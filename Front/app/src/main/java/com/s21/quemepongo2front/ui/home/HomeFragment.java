d4eefc8c102fa69bfffe251c8223374723ddc864
Alexis <alexisfonzos_@hotmail.com>
2019-11-04 16:23:09 -0300

    Quitando comentarios y metodo ineceseario

--- Front/app/src/main/java/com/s21/quemepongo2front/ui/home/HomeFragment.java
+++ Front/app/src/main/java/com/s21/quemepongo2front/ui/home/HomeFragment.java
@@ -42,19 +42,14 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
-        //TODO crear un string que contenga el token para enviarlo por este metodo
         RestClient restClient = Api.getRetrofit().create(RestClient.class);
-        //Aca creamos el objeto "llamada" el cual va a ser el endpoint a cual vamos a llamar
-        //TODO Agregar token dinamico
+
         Call<ClimaActualRs> call = restClient.recibirPronostico(3860259, MainActivity.token);
 
-        //Ejecutamos la llamada en  un thread a parte, el cual si te deja hacer modificaciones en la view
         call.enqueue(new Callback<ClimaActualRs>() {
             @Override
-            //Este es el metodo en caso que la llamada a la API devuelva algo
             public void onResponse(Call<ClimaActualRs> call, Response<ClimaActualRs> response) {
                 if (response.isSuccessful()){
-                    //Obtenemos el body de la llamada, ya parseado a una clase java
                     ClimaActualRs data = response.body();
 
                     TextView temp_actual = getView().findViewById(R.id.temperatura_actual);
@@ -78,25 +73,6 @@ public class HomeFragment extends Fragment {
 
 
         return root;
-    }
-
-    private static String readUrl(String urlString) throws Exception {
-        BufferedReader reader = null;
-        try {
-            URL url = new URL(urlString);
-            reader = new BufferedReader(new InputStreamReader(url.openStream()));
-            StringBuffer buffer = new StringBuffer();
-            int read;
-            char[] chars = new char[1024];
-            while ((read = reader.read(chars)) != -1)
-                buffer.append(chars, 0, read);
-
-            return buffer.toString();
-        } finally {
-            if (reader != null)
-                reader.close();
-        }
-
     }
 }
 
