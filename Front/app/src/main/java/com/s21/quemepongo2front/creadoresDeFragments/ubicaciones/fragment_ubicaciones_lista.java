39f9cc22137219933a333a352d587a26fe7d91f5
Alexis <alexisfonzos_@hotmaik.com>
2019-11-11 19:12:11 -0300

    Terminando el eliminar ciudad de MisCiudades

--- Front/app/src/main/java/com/s21/quemepongo2front/creadoresDeFragments/ubicaciones/fragment_ubicaciones_lista.java
+++ Front/app/src/main/java/com/s21/quemepongo2front/creadoresDeFragments/ubicaciones/fragment_ubicaciones_lista.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -32,6 +33,20 @@ public class fragment_ubicaciones_lista extends Fragment {
     private ArrayList<CiudadRs> ciudadesUsuario;
     private ListView listViewMisCiudades;
     private ArrayAdapter<CiudadRs> adapter;
+    private Button botonEliminar;
+    private TextView textViewSeleccionCiudad;
+    private Selected itemSelected;
+
+    //TODO: hacer forma mas elegante de guardar el item que se eligio
+    private class Selected{
+        public int idCIudad;
+        public int indexOfArray;
+        public Selected(int idCIudad, int indexOfArray){
+            this.idCIudad = idCIudad;
+            this.indexOfArray = indexOfArray;
+        }
+    }
+
 
     public fragment_ubicaciones_lista() {
         // Required empty public constructor
@@ -42,38 +57,52 @@ public class fragment_ubicaciones_lista extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ubicaciones_lista, container, false);
-
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        configView();
+
         mostrarCiudadesDeUsuario();
+    }
+
+    private void configView() {
+        listViewMisCiudades= getActivity().findViewById(R.id.listViewCiudadesUsuario);
+        botonEliminar = getActivity().findViewById(R.id.buttonEliminar_miCiudad);
+        textViewSeleccionCiudad = getActivity().findViewById(R.id.textView_seleccionCiudad);
+
         listViewMisCiudades.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 CiudadRs ciudad = (CiudadRs)adapterView.getItemAtPosition(i);
-                quitarCiudad(ciudad.getId(), i);
+                itemSelected = new Selected(ciudad.getId(), i);
+                textViewSeleccionCiudad.setText("Seleccionaste: " + ciudad.toString());
+            }
+        });
+        botonEliminar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //TODO: hacer el else
+                if(itemSelected != null){
+                    quitarCiudad(itemSelected.idCIudad, itemSelected.indexOfArray);
 
+                }
             }
         });
     }
 
     private void mostrarCiudadesDeUsuario(){
         RestClient restClient = Api.getRetrofit().create(RestClient.class);
-        listViewMisCiudades= getActivity().findViewById(R.id.listViewCiudadesUsuario);
 
         Call<ArrayList<CiudadRs>> ubicaciones = restClient.misCiudades(MainActivity.token);
 
         ubicaciones.enqueue(new Callback<ArrayList<CiudadRs>>() {
-
             public void onResponse(Call<ArrayList<CiudadRs>> call, Response<ArrayList<CiudadRs>> response) {
                 if (response.isSuccessful()) {
                     if (response.body() != null) {
-
                         ciudadesUsuario = response.body();
 
-                        cargarListaCiudadesUsuario();
-
+                        adapter = new ArrayAdapter<CiudadRs>(getActivity(),R.layout.item_list_ciudades,R.id.itemListaCiudades,ciudadesUsuario);
+                        listViewMisCiudades.setAdapter(adapter);
                     } else {
                         Toast.makeText(getContext(), "Ocurrio un error: No hay ciudades cargadas", Toast.LENGTH_SHORT).show();
                     }
@@ -85,25 +114,23 @@ public class fragment_ubicaciones_lista extends Fragment {
             }
         });
     }
-     //TODO; Refactotr
-    private void cargarListaCiudadesUsuario(){
-        adapter = new ArrayAdapter(getActivity(),R.layout.item_list_ciudades,R.id.itemListaCiudades,ciudadesUsuario);
-        listViewMisCiudades.setAdapter(adapter);
-    }
 
     private void quitarCiudad(int idCiudad, final int indexInAdapter){
         RestClient restClient = Api.getRetrofit().create(RestClient.class);
 
+        botonEliminar.setEnabled(false);
         restClient.borrarciudad(idCiudad, MainActivity.token).enqueue(new Callback<Void>() {
             public void onResponse(Call<Void> call, Response<Void> response) {
                 if(response.isSuccessful()){
                     ciudadesUsuario.remove(indexInAdapter);
                     adapter.notifyDataSetChanged();
+                    textViewSeleccionCiudad.setText("Seleccionaste: ");
                 }
+                botonEliminar.setEnabled(true);
             }
 
             public void onFailure(Call<Void> call, Throwable t) {
-
+                botonEliminar.setEnabled(true);
             }
         });
     }
