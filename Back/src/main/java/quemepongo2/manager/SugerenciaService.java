3f220544621e4c007601657f1213da34a1c3747f
Diego <daiko_011@hotmail.com>
2019-11-20 21:46:56 -0300

    Agregados valores a sugerencias de vestimenta
    Añadido lenguaje español a la llamada api de openWeather

--- Back/src/main/java/quemepongo2/manager/SugerenciaService.java
+++ Back/src/main/java/quemepongo2/manager/SugerenciaService.java
@@ -25,10 +25,17 @@ public class SugerenciaService {
 	private void init() {
 		//TODO: Agregar mas opciones y pasar a BD.
 		//TODO: Cargar tambien las preferncias. Ej.: si tiene protector solar que lo use.
+		//TODO: Agregar una función que utilice el cloth y una tabla de la medida de cloth que suma cada prenda para realizar un a sugerencia mas precisa.
 		sugerencias = new HashMap();
-		sugerencias.put(0.1, "Ropa interior");
-		sugerencias.put(0.5, "Remera y unos shorts");
-		sugerencias.put(1.0, "Atuendo completo. Remera, buzo, pantalones.");
+		sugerencias.put(0.1, "Utilizar la mínima cantidad de ropa posible.");
+		sugerencias.put(0.2, "Musculosa y shorts / Vestido sin mangas fino");
+		sugerencias.put(0.27, "Musculosa y pantalón fino / Vestido sin mangas grueso");
+		sugerencias.put(0.3, "Remera corta y pantalón fino / Vestido mangas cortas fino");
+		sugerencias.put(0.5, "Remera manga corta y pantalón corto / Vestido mangas largas grueso");
+		sugerencias.put(0.7, "Remera simple, sweater fino y pantalón grueso");
+		sugerencias.put(0.9, "Remera gruesa, sweater grueso y pantalón grueso");
+		//BUFANDA y GORRO
+		sugerencias.put(1.3, "Campera abrigada");
 		sugerencias.put(1.5, "Traje militar de invierno.");
 		
 	}
