1933aa56f507851353931e7c8db0ac9d74b808c9
Alexis <alexisfonzos_@hotmail.com>
2019-12-05 12:16:56 -0300

    Fix longitud null

--- Back/src/main/java/quemepongo2/api/responses/CiudadRs.java
+++ Back/src/main/java/quemepongo2/api/responses/CiudadRs.java
@@ -50,4 +50,12 @@ public class CiudadRs {
 	public void setLatitud(BigDecimal latitud) {
 		this.latitud = latitud;
 	}
+
+	public BigDecimal getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(BigDecimal longitud) {
+		this.longitud = longitud;
+	}
 }
